971deeb2cfffc5b8dcf9baa405b0d59f40478e1f
Andrey Shaurov <Andrey.Shaurov@miningelement.com>
2020-10-19 10:17:16 +0300

    feat/update build

--- static/js/build/components/button/index.js
+++
@@ -1,9 +0,0 @@
-import Button from "./button.js";
-import { render } from "../../utils/renderDOM";
-var button = new Button({
-    className: 'my-class',
-    child: 'Click me',
-});
-// app — это class дива в корне DOM
-render(".app", button);
-//# sourceMappingURL=index.js.map
--- static/js/build/models/events.js
+++
@@ -1,6 +0,0 @@
-export default {
-    SIGN_IN_INVALID: 'invalid',
-    SIGN_IN_INPUT: 'input',
-    SIGN_IN_VALIDATE: 'validate',
-};
-//# sourceMappingURL=events.js.map
